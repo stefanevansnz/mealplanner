ea78a553462de9ddf24d278bba3bacfb82977ec5
Stefan Evans <stefan.evans@consegna.cloud>
2022-02-06 18:35:56 +1300

    added theme ready for list

--- README.md
+++ README.md
@@ -6,7 +6,19 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
+
+### `nvm use 14`
 ### `yarn start`
+
+
+
+### TODO:
+
+Set up basic shopping list
+
+https://ui.docs.amplify.aws/
+
+
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import '@aws-amplify/ui-react/styles.css'
-import { AmplifyProvider } from '@aws-amplify/ui-react'
-
+import { AmplifyProvider, Button, Card, Text, Heading, Flex, Badge, createTheme, Image, View, StepperField, Rating, useTheme } from '@aws-amplify/ui-react';
 import {MealCollection, Navnavmenu, Nav, Superlist, Todo} from './ui-components'
 
-function App() {
+import { theme } from './theme';
+
+const Example = () => {
+  const { tokens } = useTheme();
   return (
-    <AmplifyProvider>
-      <Superlist width={"100vm"}/>
-      <Todo/>
-      <Nav/>      
-      <MealCollection />
-      <Navnavmenu width={"100vm"}/>      
-    </AmplifyProvider>
+    <Card>
+      <Flex direction="row" alignItems="flex-start">
+        <Image src="https://images.unsplash.com/photo-1598300042247-d088f8ab3a91?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=930&q=80"
+          width="8rem"/>
+        <Flex direction="column" gap={tokens.space.xs}>
+          <Flex direction="row">
+            <Badge variation="success">New</Badge>
+          </Flex>
+          <Heading level={3}>
+            Product title
+          </Heading>
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque risus in sem dapibus, nec vestibulum metus mattis. Mauris dignissim maximus tellus, in feugiat nibh rhoncus a.</Text>
+          <Flex direction="row" alignItems="center">
+            <Text
+              fontSize={tokens.fontSizes.large}
+              color={tokens.colors.font.secondary}>
+              $199.99
+            </Text>
+            <StepperField
+              label="Stepper"
+              defaultValue={1}
+              min={0}
+              max={10}
+              step={1}
+              labelHidden
+            />
+            <Button variation="primary">Add to cart</Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Card>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <AmplifyProvider theme={theme}>
+      <Example />
+    </AmplifyProvider>
+  )
+}
